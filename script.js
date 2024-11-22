9b41e089b5b08360d29cd454914a3cef7a12e9ff
Cephalor00t <tyagos284@gmail.com>
2024-11-22 21:47:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,4 +46,4 @@ function iniciarAudio() {
 
 // Exemplo de doações inseridas manualmente pelo dono do site
 addDonation(28); // Adicione a primeira doação de doações
-addDonation(0); // Adicione uma segunda doação de doações
+addDonation(100); // Adicione uma segunda doação de doações
